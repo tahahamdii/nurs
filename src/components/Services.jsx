2508c958d4fc5571b664bb04234caab8e2dc7439
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-11-12 18:58:17 +0100

    last edit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import './sheet.css';
+import s1 from '/src/assets/Vector.png' ; 
+import s2 from '/src/assets/Vecotr2.png' ; 
+import s3 from '/src/assets/Vector3.png' ; 
+import sp1 from '/src/assets/sp1.png' ; 
+import sp2 from '/src/assets/sp2.png' ; 
+import sp3 from '/src/assets/sp3.png' ; 
+import f1 from '/src/assets/fct1.png' ; 
+import f2 from '/src/assets/fct2.png' ; 
+import f3 from '/src/assets/fct3.png' ; 
+
+
 
 const Services = () => {
     const services = [
-        { id: 1, title: "Satisfaction De Client", image: "/src/assets/Vector.png" },
-        { id: 2, title: "Bien-être de l'Infirmier", image: "/src/assets/Vecotr2.png" },
-        { id: 3, title: "Rentabilité Hospitalière", image: "/src/assets/Vector3.png" }
+        { id: 1, title: "Satisfaction De Client", image: s1 },
+        { id: 2, title: "Bien-être de l'Infirmier", image: s2 },
+        { id: 3, title: "Rentabilité Hospitalière", image: s3 },
 
     ]
     const sponsors = [
-        { id: 1, image: "/src/assets/sp1.png" },
-        { id: 2, image: "/src/assets/sp2.png" },
-        { id: 3, image: "/src/assets/sp3.png" }
+        { id: 1, image: sp1 },
+        { id: 2, image: sp2 },
+        { id: 3, image: sp3 }
     ]
 
     const fcts = [
-        { id: 1, title: "ChronoSoins:", description: " L'intervention rapide qui compte.", image: "/src/assets/fct1.png" },
-        { id: 2, title: "ForMe Plus:", description: " L'IA optimisée au service du bien-être du patient", image: "/src/assets/fct2.png" },
-        { id: 3, title: "Opti-Tâches:", description: " L'IA qui classe efficacement les demandes des patients et optimise la répartition des tâches", image: "/src/assets/fct3.png" }
+        { id: 1, title: "ChronoSoins:", description: " L'intervention rapide qui compte.", image: f1 },
+        { id: 2, title: "ForMe Plus:", description: " L'IA optimisée au service du bien-être du patient", image: f2 },
+        { id: 3, title: "Opti-Tâches:", description: " L'IA qui classe efficacement les demandes des patients et optimise la répartition des tâches", image: f3 }
 
 
     ]
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // other server settings
+    hmr: {
+      overlay: true // or set to false as mentioned in the error message
+    }
+  }
 })
